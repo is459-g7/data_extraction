2a44af6d26fb566eceef3d57edb25eec19ce0ff7
zhouboyangg <128611386+zhouboyangg@users.noreply.github.com>
2024-11-10 23:14:56 +0800

    Update auto_copy.sql

--- auto_copy.sql
+++ auto_copy.sql
@@ -1,3 +1,5 @@
+--SQL CODE TO CREATE AUTO COPY JOB FOR S3 Integration and INGESTION INTO REDSHIFT
+
 COPY bq2_data.joined_flights_full_weather
 FROM 's3://airline-is459/data-source/for_autocopy/'
 IAM_ROLE 'arn:aws:iam::820242926303:role/service-role/AmazonRedshift-CommandsAccessRole-20241017T010122'
